3b393908dc6ea57851707568ec2ee288beeb496b
Chris <sample@mail.com>
2023-02-12 23:34:02 +0100

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 app.get('/', (req, res) => {
     res.send('hello node api')
 })
 
-app.listen(3000, ()=> {
-    console.log('node app is running on port 3000')
+
+
+
+mongoose.
+connect('mongodb+srv://chris:123admin@cluster0.g942hzz.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log('conncted to MongoDB')
+    app.listen(3000, ()=> {
+        console.log('node app is running on port 3000')
+    })
+    
+}).catch((error) => {
+    console.log(error)
 })
+``
